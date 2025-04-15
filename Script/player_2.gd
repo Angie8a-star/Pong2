f9ce1531b546d0ffb643a6866178fd152edb1b11
Angie8a-star <ochoaangie014@gmail.com>
2025-04-15 19:26:02 -0400

    se realizó el script de la colisión de pelota

--- Script/player_2.gd
+++ Script/player_2.gd
@@ -5,16 +5,15 @@ var Speed_Impulse=30
 func _physics_process(delta):
 	
 	velocity.x = 0
-	position.x= 28
+	position.x= 1130
 	
-	if (Input.is_action_pressed("P1_UP")):
+	if (Input.is_action_pressed("P2_UP")):
 		velocity.y -=Speed_Impulse
 
-	elif (Input.is_action_pressed("P1_DOWN")):
+	elif (Input.is_action_pressed("P2_DOWN")):
 		velocity.y +=Speed_Impulse
 	
 	else:
 		velocity.y=0
 		
 	move_and_slide()
-	
---
+++ pelota.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var impulse_speed=200
+var speed_increment = 10         
+var time_interval = 5              
+var timer = 0    
+
+func _ready():
+	
+	if randi()% 2== 0:
+		velocity.x += impulse_speed
+		
+	else:
+		velocity.x -= impulse_speed
+		
+	if randi()% 2== 0:
+		velocity.y += impulse_speed
+		
+	else:
+		velocity.y -= impulse_speed
+
+func _physics_process(delta):
+	
+	position += velocity * delta
+	var collision_info = move_and_collide(velocity * delta)
+	
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+	
+	timer += delta
+	
+	if timer >= time_interval:
+		velocity *= 1 + speed_increment / 100
+		
+		timer=0
